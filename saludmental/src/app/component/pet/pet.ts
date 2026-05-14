6aea23485c08a9ae1136ea57d9d77e530a946ebe
MrShel12 <br0896198361@gmail.com>
2026-05-14 16:46:07 -0600

    actualización, además primer commit del repo

--- saludmental/src/app/component/pet/pet.ts
+++ saludmental/src/app/component/pet/pet.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-pet',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './pet.html',
   styleUrl: './pet.css'
 })
-export class Pet {
+export class Pet implements OnDestroy {
 
   hunger = 70;
   happiness = 60;
@@ -18,12 +18,13 @@ export class Pet {
   animationClass = 'idle';
 
   private animationTimeout?: number;
+  private frameInterval?: number;
 
   feedPet(){
     this.hunger = this.increaseStat(this.hunger);
     this.energy = this.decreaseStat(this.energy, 5);
     this.message = 'Gracias por alimentarme.';
-    this.setPetState('/pet-happy.jpeg', 'is-feeding');
+    this.setPetState(['/pet-happy.jpeg', '/pet-playful.jpeg'], 'is-feeding');
   }
 
   petPet(){
@@ -36,21 +37,47 @@ export class Pet {
     this.energy = this.increaseStat(this.energy);
     this.hunger = this.decreaseStat(this.hunger, 5);
     this.message = 'Que buena siesta.';
-    this.setPetState('/pet-sleepy.jpeg', 'is-sleeping');
+    this.setPetState(['/pet-sleepy.jpeg', '/pet-idle.jpeg'], 'is-sleeping');
   }
 
-  private setPetState(image: string, animationClass: string){
-    this.currentImage = image;
+  ngOnDestroy(){
+    this.clearAnimationTimers();
+  }
+
+  private setPetState(image: string | string[], animationClass: string){
+    const frames = Array.isArray(image) ? image : [image];
+
+    this.clearAnimationTimers();
+
+    this.currentImage = frames[0];
     this.animationClass = animationClass;
 
-    if(this.animationTimeout){
-      window.clearTimeout(this.animationTimeout);
+    if(frames.length > 1){
+      let frameIndex = 0;
+
+      this.frameInterval = window.setInterval(() => {
+        frameIndex = (frameIndex + 1) % frames.length;
+        this.currentImage = frames[frameIndex];
+      }, 300);
     }
 
     this.animationTimeout = window.setTimeout(() => {
       this.currentImage = '/pet-idle.jpeg';
       this.animationClass = 'idle';
+      this.clearAnimationTimers();
     }, 1400);
+  }
+
+  private clearAnimationTimers(){
+    if(this.animationTimeout){
+      window.clearTimeout(this.animationTimeout);
+      this.animationTimeout = undefined;
+    }
+
+    if(this.frameInterval){
+      window.clearInterval(this.frameInterval);
+      this.frameInterval = undefined;
+    }
   }
 
   private increaseStat(value: number){
